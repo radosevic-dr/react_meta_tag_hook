f879b5533812e0406cd078dfb7640dcf48dff27f
Radosevic Dejan <126280716+radosevic-dr@users.noreply.github.com>
2023-03-12 08:23:12 +0100

    Create useMetaData.jsx

---
+++ useMetaData.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+export const useMetaTags = (metaData) => {
+  useEffect(() => {
+    // remove existing meta tags
+    const existingMetaTags = document.querySelectorAll("meta");
+    existingMetaTags.forEach((tag) => tag.parentNode.removeChild(tag));
+
+    // adding specific meta tags
+    Object.entries(metaData).forEach(([name, content]) => {
+      const tag = document.createElement("meta");
+
+      if (name.startsWith("og:")) {
+        tag.setAttribute("property", name);
+      } else {
+        tag.setAttribute("name", name);
+      }
+
+      tag.setAttribute("content", content);
+      document.head.appendChild(tag);
+    });
+
+    return () => {
+      // remove meta tags when component unmounts
+      const existingMetaTags = document.querySelectorAll("meta");
+      existingMetaTags.forEach((tag) => tag.parentNode.removeChild(tag));
+    };
+  }, [metaData]);
+};
